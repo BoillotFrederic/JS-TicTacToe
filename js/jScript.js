2407568d2b3c4d1e308d035de6eae103cd3812e5
BoillotFrederic <boillot.frederic.62@gmail.com>
2016-11-29 11:50:04 +0100

    JS

--- js/jScript.js
+++ js/jScript.js
@@ -1,3 +1,12 @@
+// Simplification
+function $(id, n)
+{
+  if (n >= 0)
+  return document.getElementsByTagName(id)[n];
+  else
+  return document.getElementById(id);
+}
+
 // Le jeu
 var game =
 {
@@ -22,8 +31,8 @@ var game =
 
     this.crossOrCircle = true;
     this.loop = 0;
-    document.getElementsByTagName('table')[0].onclick = function(e) { game.pushCrossOrCircle(e); };
-    this.cpu = document.getElementById('cpu').checked;
+    $('table', 0).onclick = function(e) { game.pushCrossOrCircle(e); };
+    this.cpu = $('cpu').checked;
 
     var td = document.getElementsByTagName('td');
 
@@ -33,17 +42,17 @@ var game =
       td[i].innerHTML = '';
     }
 
-    document.getElementsByTagName('h2')[0].innerHTML = 'Au player 1 de jouer';
-    document.getElementsByTagName('h2')[0].style.color = 'white';
+    $('h2', 0).innerHTML = 'Au player 1 de jouer';
+    $('h2', 0).style.color = 'white';
 
     for (var i = 0; i < 9; i++)
-    document.getElementsByTagName('td')[i].style.backgroundColor = 'transparent';
+    $('td', i).style.backgroundColor = 'transparent';
   },
 
   // Fin de la partie
   stop: function()
   {
-    document.getElementsByTagName('table')[0].onclick = '';
+    $('table', 0).onclick = '';
   },
 
   // Ajout de la croix ou du rond
@@ -64,7 +73,7 @@ var game =
         this.crossOrCircle = 1;
 
         // A qui le tour
-        document.getElementsByTagName('h2')[0].innerHTML = 'Au player 1 de jouer';
+        $('h2', 0).innerHTML = 'Au player 1 de jouer';
 
         // Contrôle des lignes gagnantes
         if (this.check()) return;
@@ -80,7 +89,7 @@ var game =
         this.crossOrCircle = 0;
 
         // A qui le tour
-        document.getElementsByTagName('h2')[0].innerHTML = 'Au '+ (this.cpu ? 'CPU' : 'player 2') +' de jouer';
+        $('h2', 0).innerHTML = 'Au '+ (this.cpu ? 'CPU' : 'player 2') +' de jouer';
 
         // Contrôle des lignes gagnantes
         if (this.check()) return;
@@ -94,7 +103,7 @@ var game =
         }
       }
 
-      document.getElementsByTagName('h2')[0].style.color = 'white';
+      $('h2', 0).style.color = 'white';
     }
   },
 
@@ -112,7 +121,7 @@ var game =
         // Mise en évidence de la ligne gagnante
         var color = !testLine ? 'rgba(255, 0, 0, 0.2)' : 'rgba(0, 255, 0, 0.2)';
         for (var i2 = 0; i2 < 3; i2++)
-        document.getElementsByTagName('td')[order[i2]].style.backgroundColor = color;
+        $('td', order[i2]).style.backgroundColor = color;
 
         // Arrêt du jeu
         this.stop();
@@ -123,7 +132,7 @@ var game =
           var msg = 'Victoire du player 1 !!!';
           color = 'red';
           this.scoreP1++;
-          document.getElementsByTagName('em')[0].innerHTML = this.scoreP1;
+          $('em', 0).innerHTML = this.scoreP1;
         }
         else
         {
@@ -133,17 +142,17 @@ var game =
           if (this.cpu)
           {
             this.scoreCPU++;
-            document.getElementsByTagName('em')[2].innerHTML = this.scoreCPU;
+            $('em', 2).innerHTML = this.scoreCPU;
           }
           else
             {
               this.scoreP2++;
-              document.getElementsByTagName('em')[1].innerHTML = this.scoreP2;
+              $('em', 1).innerHTML = this.scoreP2;
             }
         }
 
-        document.getElementsByTagName('h2')[0].innerHTML = msg;
-        document.getElementsByTagName('h2')[0].style.color = color;
+        $('h2', 0).innerHTML = msg;
+        $('h2', 0).style.color = color;
 
         return true;
       }
@@ -154,9 +163,9 @@ var game =
 
     if (this.loop >= 9)
     {
-      document.getElementsByTagName('h2')[0].innerHTML = 'Match nul !!!';
+      $('h2', 0).innerHTML = 'Match nul !!!';
       this.scoreNul++;
-      document.getElementsByTagName('em')[3].innerHTML = this.scoreNul;
+      $('em', 3).innerHTML = this.scoreNul;
     }
   },
 
@@ -164,13 +173,13 @@ var game =
   cpuBot: function(This)
   {
     var i = Math.floor(Math.random() * 9);
-    var td = document.getElementsByTagName('td')[i];
+    var td = $('td', i);
 
     if (This.casePush[i] == 9)
     {
       clearInterval(this.timer);
+      $('table', 0).onclick = function(e) { game.pushCrossOrCircle(e); };
       This.pushCrossOrCircle(td);
-      document.getElementsByTagName('table')[0].onclick = function(e) { game.pushCrossOrCircle(e); };
     }
     else
     This.cpuBot(This);
